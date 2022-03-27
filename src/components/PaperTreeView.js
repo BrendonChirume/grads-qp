12c37352aa966ca79cf4577ed80d1e2df7d986a8
Brendon Chirume <tchirume14@gmail.com>
2022-03-27 21:20:03 +0200

    WP: Treeview

--- src/components/PaperTreeView.js
+++ src/components/PaperTreeView.js
@@ -1,67 +1,182 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
-import ListTree from './ListTree';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import TreeView from '@mui/lab/TreeView';
+import TreeItem, { treeItemClasses } from '@mui/lab/TreeItem';
+import Typography from '@mui/material/Typography';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import FolderIcon from '@mui/icons-material/Folder';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import { useRouter } from 'next/router';
 
 const data = [
   {
-    'Year 1': [
+    name: 'Year 1',
+    children: [
       {
         name: 'Semester 1',
         children: [
-          'Linear Algebra',
-          'Calculas',
-          'Infomation Management',
-          'Communication Skills',
-          'Electronic Commerce',
-          'Database Systems'
+          { name: 'Linear Algebra' },
+          { name: 'Calculas' },
+          { name: 'Infomation Management' },
+          { name: 'Communication Skills' },
+          { name: 'Electronic Commerce' },
+          { name: 'Database Systems' }
         ]
       },
       {
         name: 'Semester 2',
         children: [
-          'Descrete Mathematics',
-          'Data Mining',
-          'Introduction to Programming Consepts in Python',
-          'Business Information Systems',
-          'Applied Statistics',
-          'Data Visualisation'
+          { name: 'Descrete Mathematics' },
+          { name: 'Data Mining' },
+          { name: 'Introduction to Programming Consepts in Python' },
+          { name: 'Business Information Systems' },
+          { name: 'Applied Statistics' },
+          { name: 'Data Visualisation' }
         ]
       }
     ]
   },
   {
-    'Year 2': [
+    name: 'Year 2',
+    children: [
       {
         name: 'Semester 1',
         children: [
-          'Technopreneureship',
-          'Research Methods',
-          'Data Analysis & Simulation',
-          'Statistical Programming',
-          'Application Development',
-          'Data Communication & Network'
+          { name: 'Technopreneureship' },
+          { name: 'Research Methods' },
+          { name: 'Data Analysis & Simulation' },
+          { name: 'Statistical Programming' },
+          { name: 'Application Development' },
+          { name: 'Data Communication & Network}' }
         ]
       },
       {
         name: 'Semester 2',
         children: [
-          'Decision Support Systems',
-          'Parallel & Distributed Processing',
-          'Data Science Project',
-          'Information Security & Auditing',
-          'Molecular Biology',
-          'Data Analytics',
-          'Group Project'
+          { name: 'Decision Support Systems' },
+          { name: 'Parallel & Distributed Processing' },
+          { name: 'Data Science Project' },
+          { name: 'Information Security & Auditing' },
+          { name: 'Molecular Biology' },
+          { name: 'Data Analytics' },
+          { name: 'Group Project}' }
         ]
       }
     ]
   }
 ];
 
-export default function PaperTreeView() {
-  const handleSelect = (event, nodeIds) => {};
+const StyledTreeItemRoot = styled(TreeItem)(({ theme }) => ({
+  color: theme.palette.text.secondary,
+  [`& .${treeItemClasses.content}`]: {
+    color: theme.palette.text.secondary,
+    borderTopRightRadius: theme.spacing(2),
+    borderBottomRightRadius: theme.spacing(2),
+    paddingRight: theme.spacing(1),
+    fontWeight: theme.typography.fontWeightMedium,
+    '&.Mui-expanded': {
+      fontWeight: theme.typography.fontWeightRegular
+    },
+    '&:hover': {
+      backgroundColor: theme.palette.action.hover
+    },
+    '&.Mui-focused, &.Mui-selected, &.Mui-selected.Mui-focused': {
+      backgroundColor: `var(--tree-view-bg-color, ${theme.palette.action.selected})`,
+      color: 'var(--tree-view-color)'
+    },
+    [`& .${treeItemClasses.label}`]: {
+      fontWeight: 'inherit',
+      color: 'inherit'
+    }
+  },
+  [`& .${treeItemClasses.group}`]: {
+    marginLeft: 0,
+    [`& .${treeItemClasses.content}`]: {
+      paddingLeft: theme.spacing(2)
+    }
+  }
+}));
 
-  return data.map((node, index) => <ListTree key={index} node={node} />);
+function StyledTreeItem(props, ref) {
+  const {
+    bgColor,
+    color,
+    labelIcon: LabelIcon,
+    labelInfo,
+    labelText,
+    ...other
+  } = props;
+
+  return (
+    <StyledTreeItemRoot
+      label={
+        <Box sx={{ display: 'flex', alignItems: 'center', p: 0.5, pr: 0 }}>
+          <Box component={LabelIcon} color="inherit" sx={{ mr: 1 }} />
+          <Typography variant="body2" sx={{ fontWeight: 'inherit', flexGrow: 1 }}>
+            {labelText}
+          </Typography>
+          <Typography variant="caption" color="inherit">
+            {labelInfo}
+          </Typography>
+        </Box>
+      }
+      style={{
+        '--tree-view-color': color,
+        '--tree-view-bg-color': bgColor
+      }}
+      {...other}
+    />
+  );
+}
+
+StyledTreeItem.propTypes = {
+  bgColor: PropTypes.string,
+  color: PropTypes.string,
+  labelIcon: PropTypes.elementType.isRequired,
+  labelText: PropTypes.string.isRequired
+};
+
+const renderTree = ({ name, children }, rootID, path = ['']) => {
+  return (
+    <StyledTreeItem
+      path={path}
+      underline="none"
+      key={rootID}
+      nodeId={`${rootID}`}
+      labelText={name}
+      labelIcon={FolderIcon}
+    >
+      {Array.isArray(children)
+        ? children.map((node, childID) =>
+            renderTree(node, `${rootID}.${childID}`, `${path}/${node.name}`)
+          )
+        : null}
+    </StyledTreeItem>
+  );
+};
+
+export default function PaperTreeView() {
+  const { push } = useRouter();
+  const handleSelect = ({ currentTarget }, nodeIds) => {
+    const path = `/${currentTarget.parentNode
+      .getAttribute('path')
+      .replace(/ /g, '-')}`;
+    console.log(path);
+    push(path);
+  };
+
+  return (
+    <TreeView
+      defaultCollapseIcon={<KeyboardArrowDownIcon fontSize="small" />}
+      defaultExpandIcon={<NavigateNextIcon fontSize="small" />}
+      onNodeSelect={handleSelect}
+      sx={{ flexGrow: 1, maxWidth: 400, overflowY: 'auto', height: '100%' }}
+    >
+      {data.map((node, index) => renderTree(node, index, node.name))}
+    </TreeView>
+  );
 }
 
 PaperTreeView.propTypes = {};
